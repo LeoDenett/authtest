152f8bf6a3316992dd3a320bf5223ed3ce8260a4
Leonel Denett <leoneldenett@hotmail.com>
2022-05-06 15:02:44 +0200

    add json_login in Security.yaml

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { render } from "react-dom";
+import { Formik } from 'formik';
 import './styles/app.css';
 import axios from "axios";
 
@@ -28,6 +29,67 @@ export default function App() {
                     );
                 })
             }
+
+            <h1>Login by React</h1>
+
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                validate={values => {
+                    const errors = {};
+                    if (!values.email) {
+                        errors.email = 'Required';
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                        errors.email = 'Invalid email address';
+                    }
+                    return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
+                {({
+                      values,
+                      errors,
+                      touched,
+                      handleChange,
+                      handleBlur,
+                      handleSubmit,
+                      isSubmitting,
+                      /* and other goodies */
+                  }) => (
+
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="email"
+                            name="email"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            placeholder="Email"
+                        />
+
+                        {errors.email && touched.email && errors.email}
+
+                        <input
+                            type="password"
+                            name="password"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            placeholder="Password"
+                        />
+                        {errors.password && touched.password && errors.password}
+                        <button type="submit" disabled={isSubmitting}>
+                            Submit
+                        </button>
+                    </form>
+                )}
+            </Formik>
         </div>
     );
 }
